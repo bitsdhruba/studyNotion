f18ca4c2620045d93eadc3cdc392c4740f64ada6
bitsdhruba <142327840+bitsdhruba@users.noreply.github.com>
2023-12-06 22:57:28 +0530

    fixed privateRoute.

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 
  function PrivateRoute  ({loggedin, children}) {
 
-        if(loggedin){children}
+        if(loggedin){return children}
         else{<Navigate to="/login" />}
 }
 
